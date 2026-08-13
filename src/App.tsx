d6cf1a8e6ce4d92c7c8e21a5f574d14ad40aed16
Arush Wavhal <arushwavhalawm@gmail.com>
2026-08-13 11:32:52 +0530

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { NeonBackground } from "@/components/party/NeonBackground";
+import { Navbar } from "@/components/party/Navbar";
+import { Hero } from "@/components/party/Hero";
+import { About } from "@/components/party/About";
+import { Timeline } from "@/components/party/Timeline";
+import { Gallery } from "@/components/party/Gallery";
+import { Competitions } from "@/components/party/Competitions";
+import { OrganizingTeam } from "@/components/party/OrganizingTeam";
+import { Faqs } from "@/components/party/Faqs";
+import { Footer } from "@/components/party/Footer";
+
+function App() {
+  return (
+    <>
+      <NeonBackground />
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Timeline />
+        <Gallery />
+        <Competitions />
+        <OrganizingTeam />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
